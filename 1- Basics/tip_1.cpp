516abbcde5b6e71a6ac40112da33e6bbed177207
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2017-08-12 21:18:39 +0200

    up!

---
+++ 1- Basics/tip_1.cpp
@@ -0,0 +1,23 @@
+/**
+ * ASCII tips
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<(char)48<<"\n";	// 0
+	cout<<(int)'0'<<"\n";	// 48
+
+	cout<<(char)65<<"\n";	// A
+	cout<<(int)'A'<<"\n";	// 65
+	cout<<(int)'B'<<"\n";	// 66
+	cout<<(int)'C'<<"\n";	// 67
+
+	cout<<(char)97<<"\n";	// a
+	cout<<(int)'a'<<"\n";	// 97
+
+	cout<<(char)35<<"\n";	// #
+	cout<<(char)043<<"\n";	// #
+	cout<<(char)0x23<<"\n";	// #
+	return 0;
+}
