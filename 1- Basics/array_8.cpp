485803948222f3fa0c6294439f6ac23da2177673
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2017-08-16 08:46:28 +0200

    Arrays: find the second maximum number
    Given array of distinct number, find the second maximum number.

---
+++ 1- Basics/array_8.cpp
@@ -0,0 +1,33 @@
+/**
+ * Given array of distinct number, find the second maximum number.
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int AS = 5;
+	int arr[AS] = {6,2,30,4,5};
+
+	int iFstMax = arr[0],		// First Max Num
+			iSndMax = arr[1];	// Second Max Num
+
+	if(iSndMax > iFstMax) {
+		int i = iFstMax;
+		iFstMax = iSndMax;
+		iSndMax = i;
+	}
+
+	for(int i=2; i < AS; i++) {
+		if( arr[i] > iFstMax ) {
+			int x = iFstMax;
+			iFstMax = arr[i];
+			iSndMax = x;
+		} else if ( arr[i] < iFstMax && arr[i] > iSndMax) {
+			iSndMax = arr[i];
+		}
+	}
+
+	cout<< "First Max Num: " << iFstMax <<"\nSecond Nax Num: " << iSndMax;
+
+	return 0;
+}
