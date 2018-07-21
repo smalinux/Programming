cb37353fb25d2f3eb018d749ce8145117eadbc1c
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2018-07-21 14:36:20 +0200

    tips & tricks - Char data type
    الملف دا هحاول احط فيه كل التريكات اللى اعرفها و اللعب اللى بيحصل مع الـ
    char data type

---
+++ 1- Basics/tips & tricks - Char data type.cpp
@@ -0,0 +1,32 @@
+//============================================================================
+// Name        : test.cpp
+// Author      : smalinux
+// local	   : D:\__CPP\eclipseWorkspace\generalTest-forLearningCPP\src
+// Description : <tips & tricks> Char data type.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+	// -------------------------------------------------------------------
+
+	char a = '(',
+			b = 40,
+			c = 0x28,
+			d = 050;
+
+	cout<<"a = " << a << endl;
+	cout<<"b = " << b << endl;
+	cout<<"c = " << c << endl;
+	cout<<"d = " << d << endl;
+
+	// -------------------------------------------------------------------
+
+	// Can I use a binary literal in C or C++?
+	// int x = 00010000;
+
+	// -------------------------------------------------------------------
+	return 0;
+}
