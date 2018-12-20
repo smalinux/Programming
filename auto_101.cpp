9457d9919bc388b4096e10a80eb4f14be4c29342
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2018-12-20 19:52:11 +0200

    auto keyword

---
+++ auto_101.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// ----------------------------------------------------------------------
+
+	auto x = 100;
+	auto y = 10.5;
+	auto z = "Sohaib";
+
+	cout << x << "\n"
+			<< y << "\n"
+			<< z;
+
+	// ----------------------------------------------------------------------
+	return 0;
+}
+
+/*
+ * Conditions:
+ * 	- init. and declaration MUST be in the same line
+ */
