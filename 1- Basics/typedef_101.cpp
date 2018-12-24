8f41c270c9957cbe20b9288fc98e6438007a54aa
Sohaib Mohammed <sohaibfacebook396@gmail.com>
2018-12-24 07:02:03 +0200

    up

---
+++ 1- Basics/typedef_101.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// ----------------------------------------------------------------------
+
+	typedef unsigned long long int ull;		// create new alias named "ull" from "unsigned long long int"
+
+	ull x = 50;
+
+	cout << x;
+
+	// ----------------------------------------------------------------------
+	return 0;
+}
